2b957738c541724bc6d0c35d8a0e50ddbaccef3a
Dmitry-Shagojko <dmitry2844@hotmail.com>
2022-07-27 01:43:03 +0300

    тест доступа

--- src/Main.java
+++ src/Main.java
@@ -74,5 +74,6 @@ public class Main {
 
         MethodistServices methodistServices = new MethodistServices();
         methodistServices.conversionClass(listAllTeachers.teacherRepository.teachers.get(8));   //конвертация учителя в методиста
+        System.out.println("Test");
     }
 }
